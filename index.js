fe2489af75294f39b3e3ea53d21f9b2a609b1167
Michael Okello <okellofrost@gmail.com>
2022-07-18 14:08:20 +0300

    refactor for prototypes

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const { timeline } = require("animejs");
+//const { timeline } = require("animejs");
 
-function stopwatch(){
+function Stopwatch(){
     let startTime , endTime , running , duration = 0;
 
-    this.start = function(){
-        if(running)
-        throw new Error('Stopwatch has already started.');
-        running =  true;
-        startTime = new Date();
-    };
-    
-    this.stop = function(){
-        if(!running)
-        throw new Error('Stopwatch is not started.');
-        running = false;
-        endTime =new Date();
-
-        const seconds = (endTime.getTime() -startTime()) / 1000;
-        duration =+duration;
-    };
-    this.reset = function(){
-        startTime = null;
-        endTime = null;
-        duration = 0;
-    };
+   
     Object.defineProperty(this, 'duration',{
         get: function(){return duration;}
     });
 }
+
+const watch = new Stopwatch();
+ console.log(watch.start());
+
+ Stopwatch.prototype.start = function(){
+    if(running)
+    throw new Error('Stopwatch has already started.');
+    running =  true;
+    startTime = new Date();
+};
+
+Stopwatch.prototype.stop = function(){
+    if(!running)
+    throw new Error('Stopwatch is not started.');
+    running = false;
+    endTime =new Date();
+
+    const seconds = (endTime.getTime() -startTime()) / 1000;
+    duration =+duration;
+};
+Stopwatch.prototype.reset = function(){
+    startTime = null;
+    endTime = null;
+    duration = 0;
+};
